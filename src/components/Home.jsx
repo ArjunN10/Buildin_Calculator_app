76cb59a9238393fa7e156c6800985843ebfd596f
ARJUN N <138310139+ArjunN10@users.noreply.github.com>
2023-10-06 10:45:54 +0530

    calculator_app

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,20 +1,21 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 export const Home = () => {
-
-  const [input, setInput] = useState('');
-  const [result, setResult] = useState('');
+  const [input, setInput] = useState("");
+  const [result, setResult] = useState("");
 
   const handleClick = (value) => {
-    if (value === '=') {
+    if (value === "=") {
       try {
-        setResult(eval(input));
+        // Replace "^" with "**" for the power operation
+        const modifiedInput = input.replace("^", "**");
+        setResult(eval(modifiedInput));
       } catch (error) {
-        setResult('Error');
+        setResult("Error");
       }
-    } else if (value === 'C') {
-      setInput('');
-      setResult('');
+    } else if (value === "C") {
+      setInput("");
+      setResult("");
     } else {
       setInput((prevInput) => prevInput + value);
     }
@@ -22,32 +23,33 @@ export const Home = () => {
 
   return (
     <div>
-        <div className="calculator">
-      <div className="display">
-        <input type="text" value={input} readOnly />
-        <span className="result">{result}</span>
-      </div>
-      <div className="buttons">
-        <button onClick={() => handleClick('9')}>9</button>
-        <button onClick={() => handleClick('8')}>8</button>
-        <button onClick={() => handleClick('7')}>7</button>
-        <button onClick={() => handleClick('6')}>6</button>
-        <button onClick={() => handleClick('5')}>5</button>
-        <button onClick={() => handleClick('4')}>4</button>
-        <button onClick={() => handleClick('3')}>3</button>
-        <button onClick={() => handleClick('2')}>2</button>
-        <button onClick={() => handleClick('1')}>1</button>
-        <button onClick={() => handleClick('0')}>0</button>
-        <button onClick={() => handleClick('+')}>+</button>
-        <button onClick={() => handleClick('-')}>-</button>
-        <button onClick={() => handleClick('*')}>*</button>
-        <button onClick={() => handleClick('.')}>.</button>
-        <button onClick={() => handleClick('=')}>=</button>
-        <button onClick={() => handleClick('C')}>C</button>
-        <button onClick={() => handleClick('/')}>/</button>
+      <div className="calculator">
+        <div className="display">
+          <input type="text" value={input} placeholder="Calculator" readOnly />
+          <span className="result">{result}</span>
+        </div>
+        <div className="buttons">
+          <button onClick={() => handleClick("+")}>+</button>
+          <button onClick={() => handleClick("-")}>-</button>
+          <button onClick={() => handleClick("*")}>*</button>
+          <button onClick={() => handleClick("/")}>/</button>
+          <button onClick={() => handleClick("%")}>%</button>
+          <button onClick={() => handleClick("^")}>^</button> 
+          <button onClick={() => handleClick("C")}>C</button>
+          <button onClick={() => handleClick("=")}>=</button>
+          <button onClick={() => handleClick("9")}>9</button>
+          <button onClick={() => handleClick("8")}>8</button>
+          <button onClick={() => handleClick("7")}>7</button>
+          <button onClick={() => handleClick("6")}>6</button>
+          <button onClick={() => handleClick("5")}>5</button>
+          <button onClick={() => handleClick("4")}>4</button>
+          <button onClick={() => handleClick("3")}>3</button>
+          <button onClick={() => handleClick("2")}>2</button>
+          <button onClick={() => handleClick("1")}>1</button>
+          <button onClick={() => handleClick("0")}>0</button>
+          <button onClick={() => handleClick(".")}>.</button>
+        </div>
       </div>
     </div>
-  
-    </div>
-  )
-}
+  );
+};
