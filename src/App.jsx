7d1495fa56c55d8854e83ec5307eb1513cb3800b
ARJUN N <138310139+ArjunN10@users.noreply.github.com>
2023-10-06 10:13:53 +0530

    calculator

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import { Home } from './components/Home';
+
+function App() {
+  return (
+    <>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+    </Routes>
+
+
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Home.jsx
@@ -0,0 +1,58 @@
+import React, { useState } from 'react'
+
+export const Home = () => {
+
+      // State to store the input and result
+  const [input, setInput] = useState('');
+  const [result, setResult] = useState('');
+
+  // Function to handle button clicks
+  const handleClick = (value) => {
+    if (value === '=') {
+      try {
+        // Evaluate the expression and set the result
+        setResult(eval(input));
+      } catch (error) {
+        setResult('Error');
+      }
+    } else if (value === 'C') {
+      // Clear the input and result
+      setInput('');
+      setResult('');
+    } else {
+      // Append the clicked value to the input
+      setInput((prevInput) => prevInput + value);
+    }
+  };
+
+  return (
+    <div>
+        <div className="calculator">
+      <div className="display">
+        <input type="text" value={input} readOnly />
+        <span className="result">{result}</span>
+      </div>
+      <div className="buttons">
+        <button onClick={() => handleClick('9')}>9</button>
+        <button onClick={() => handleClick('8')}>8</button>
+        <button onClick={() => handleClick('7')}>7</button>
+        <button onClick={() => handleClick('6')}>6</button>
+        <button onClick={() => handleClick('5')}>5</button>
+        <button onClick={() => handleClick('4')}>4</button>
+        <button onClick={() => handleClick('3')}>3</button>
+        <button onClick={() => handleClick('2')}>2</button>
+        <button onClick={() => handleClick('1')}>1</button>
+        <button onClick={() => handleClick('0')}>0</button>
+        <button onClick={() => handleClick('+')}>+</button>
+        <button onClick={() => handleClick('-')}>-</button>
+        <button onClick={() => handleClick('*')}>*</button>
+        <button onClick={() => handleClick('.')}>.</button>
+        <button onClick={() => handleClick('=')}>=</button>
+        <button onClick={() => handleClick('C')}>C</button>
+        <button onClick={() => handleClick('/')}>/</button>
+      </div>
+    </div>
+  
+    </div>
+  )
+}
